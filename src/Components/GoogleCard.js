aa07c2f5ae673f890b5d362ca24a32947625a45a
Ali İhsan <ihsanozkaya1903@gmail.com>
2023-03-20 21:13:24 +0300

    Personel Geldi

--- src/Components/GoogleCard.js
+++ src/Components/GoogleCard.js
@@ -11,28 +11,48 @@ const AnyReactComponent = ({ area, text }) => (
         <p>Boylam: {area.coordinates.longitude}</p>
         <p>
           Gerekli ürünler:{" "}
-          {area.requrired_products.length === 0 ? (
+          {area.requrired_products.length === 0 ? 
             "İhtiyaç yok"
-          ) : (
+           : (
             <div style={{flexDirection: "column"}}>
-              {area.requrired_products.map((product) => (
-                <div className="d-flex justify-content Center flex-wrap">
+              {area.requrired_products.map((product, i) => (
+                <div key={i} className="d-flex justify-content Center flex-wrap">
                   <Badge 
                     color="green"
                     count={product.quantity}
-                    className="mx-0 my-0"
+                    className="mx-0 my-0 p-0"
                   >
                      </Badge>
-                     <Tag color="blue" className="mx-0 my-2">
+                     <Tag color="blue" className="mx-0 my-2 p-1">
                       {product.Product.title}
                     </Tag>
                 </div>
               ))}
             </div>
           )}
-          {" \n"}
         </p>
-        <p>Gerekli görevliler:</p>
+        <p>
+          Gerekli Personel: {" "}
+          {area.requrired_people.length === 0 ? 
+            "İhtiyaç yok"
+           : (
+            <div style={{flexDirection: "column"}}>
+              {area.requrired_people.map((person, i) => (
+                <div key={i} className="d-flex justify-content Center flex-wrap">
+                  <Badge 
+                    color="green"
+                    count={person.quantity}
+                    className="mx-0 my-0 p-0"
+                  >
+                     </Badge>
+                     <Tag color="blue" className="mx-0 my-2 p-1">
+                      {person.Person.name}
+                    </Tag>
+                </div>
+              ))}
+            </div>
+          )}
+        </p>
       </div>
     }
     title={text}
