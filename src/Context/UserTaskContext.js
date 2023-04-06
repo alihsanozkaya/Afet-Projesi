ef6fabdfaa9b66213c7fa2901911ed303d1913fa
Ali İhsan <ihsanozkaya1903@gmail.com>
2023-04-06 14:22:11 +0300

    son

--- src/Context/UserTaskContext.js
+++ src/Context/UserTaskContext.js
@@ -9,20 +9,23 @@ export const UserTaskContextProvider = ({children}) => {
     const [user, setUser] = useState(  JSON.parse(localStorage.getItem("user")) || {});
 
     useEffect(() => {
-      async function fetchData(){
-        await axios
-            .get(`https://afetapi.onrender.com/api/users/${user._id}/tasks`)
-            .then((res) => {
-                setData(res.data)
-                setLoading(false)
-            })
-            .catch((error) => {
-                console.log(error)
-                setLoading(false)
-            })
-      }
-      fetchData()
-    }, [data])
+        if(user !== {})
+        {
+            async function fetchData(){
+                await axios
+                    .get(`https://afetapi.onrender.com/api/users/${user._id}/tasks`)
+                    .then((res) => {
+                        setData(res.data)
+                        setLoading(false)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                        setLoading(false)
+                    })
+              }
+              fetchData()
+        }
+    }, [user,data])
     if(loading){
         return console.log("Veriler yükleniyor")
     }
